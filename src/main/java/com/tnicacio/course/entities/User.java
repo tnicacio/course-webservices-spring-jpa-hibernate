0ea9b7167940a9d17ce03c0f20735d9f31a6aad4
tnicacio <nicacio.t92@gmail.com>
2021-02-13 18:42:00 -0300

    order, instant part1

--- src/main/java/com/tnicacio/course/entities/User.java
+++ src/main/java/com/tnicacio/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.tnicacio.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User implements Serializable{
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -18,6 +23,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -69,6 +77,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
